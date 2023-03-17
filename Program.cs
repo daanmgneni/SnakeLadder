382b5803083ead38f8bc002bd6dd78faba10a11b
DAAN <daanmgneni@gmail.com>
2023-03-17 20:15:21 +0530

    Main program added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeAndLeaderProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Leader Game!!!");
+
+        }
+
+    }
+}
