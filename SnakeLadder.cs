1cdf38240698acfcd5277c9db498e079f1bc2076
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-03-17 20:26:24 +0530

    Create SnakeLadder.cs

---
+++ SnakeLadder.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLeaderProblem
+{
+    internal class SnakeLeaderCode
+    {
+        public static void call()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            Console.WriteLine("Single Player at Start Position 0");
+        }
+    }
+}
